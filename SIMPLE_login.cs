e80fb4fee7dd698c02cbd2b0833076b748249f73
Adamoss <95488005+QadamosssQ@users.noreply.github.com>
2022-08-28 12:18:03 +0200

    THE SIMPLEST login i can make.

---
+++ SIMPLE_login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Login:");
+            string login = Console.ReadLine();
+            Console.WriteLine("Passowrd:");
+            string password = Console.ReadLine();
+
+            if (login == "adamoss")
+            {
+                if (password == "321")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Logged");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password");
+                    Console.WriteLine("Or can not find login ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("can not find");
+            }
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+//ctrl + k   ctrl + D
+//tab+ tab po: //  cw  Console.WriteLine(); //  for for (int i = 0; i < length; i++) // while...
